ffb7c67921c6026157fcb3da4e5b876e0129cafe
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2022-11-19 19:16:49 +0500

    Problem 1 updated

---
+++ Loops Solution/Problem1/problem1.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(N+" x "+(i+1)+" = "+(N*(i+1)));
+        }
+    }
+}
+
+/*   -------- 0utput ------
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+*/
+
