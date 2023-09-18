a4f217e72d921800ff0c7dda48160dd90b721261
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-09-18 11:36:54 +0500

    Update Main.java

--- Drawing-Book Problem/Main.java
+++ Drawing-Book Problem/Main.java
@@ -1,23 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("git checkout main");
-<<<<<<< HEAD
-    }
-    public static int pageCount(int n, int p) {
-        int diff = n - p, turns = 0;
-
-
-
-        return turns;
+        System.out.println(pageCount(5,2));
 
     }
 
-=======
-    }   
     public static int pageCount(int n, int p) {
 
         return Math.min(p/2, n/2 - p/2);
 
     }
->>>>>>> 6cb578dcc1d2f71cc5dff68df36165469cb3650a
+
 }
