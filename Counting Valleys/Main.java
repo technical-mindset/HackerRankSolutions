d5efae2a9ff66cf6f57d61bdfd0d4f629d1e8366
Asad Zaidi <asadzaidi201999@gmail.com>
2023-09-21 20:13:56 +0500

    Counting Valley's Problem inital completed

---
+++ Counting Valleys/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(countingValleys(18,"DDUDUDDUDDUDDUUUUD"));
+    }
+
+    public static int countingValleys(int steps, String path) {
+        int upAndDown = 0, valleys = 0;
+        for (int i = 0; i < steps; i++) {
+            if (path.charAt(i) =='U') {
+                upAndDown++;
+            }
+            else if (path.charAt(i) =='D'){
+                upAndDown--;
+                if (upAndDown == 0) {
+                    valleys++;
+                }
+
+            }
+        }
+        for (char c: path.toCharArray()) {
+
+            System.out.println(c);
+        }
+
+
+        return valleys;
+
+    }
+}
