987e4b322005edf927c764471722873c8220866f
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-09-21 20:57:18 +0500

    Electrnoics Shop Problem Uploaded

--- Electronic Shops/Main.java
+++ Electronic Shops/Main.java
@@ -8,10 +8,14 @@ public class Main {
     }
     static int getMoneySpent(int[] keyboards, int[] drives, int b) {
         int purchased = -1, temp = 0;
+        
         for (int i = 0; i < keyboards.length; i++) {
+        
             for (int j = 0; j < drives.length; j++) {
                 temp = keyboards[i] + drives[j];
+                
                 if (temp <= b) {
+                    
                     if (temp > purchased) {
                         purchased = temp;
                     }
